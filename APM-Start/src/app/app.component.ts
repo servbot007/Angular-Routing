fe506789de28458a2e01ac34f3699583c69371d7
Paul Askegard <angry.twinky@hotmail.com>
2018-03-21 22:57:28 -0500

    Module 10
    Module 10

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 import { AuthService } from './user/auth.service';
+import { MessageService } from './messages/message.service';
 
 @Component({
     selector: 'pm-app',
@@ -11,7 +12,7 @@ export class AppComponent {
     pageTitle: string = 'Acme Product Management';
     loading: boolean = true;
 
-    constructor(private authService: AuthService, private router: Router) {
+    constructor(private authService: AuthService, private router: Router, private messageService: MessageService) {
         router.events.subscribe(
             (routerEvent: Event) => {
                 this.checkRouterEvent(routerEvent);
@@ -27,8 +28,18 @@ export class AppComponent {
         if (routerEvent instanceof NavigationEnd ||
             routerEvent instanceof NavigationCancel ||
             routerEvent instanceof NavigationError) {
-                this.loading = false;
-            }
+            this.loading = false;
+        }
+    }
+
+    displayMessages(): void {
+        this.router.navigate([{ outlets: { popup: ['messages'] } }]);
+        this.messageService.isDisplayed = true;
+    }
+
+    hideMessages(): void {
+        this.router.navigate([{ outlets: { popup: null } }]);
+        this.messageService.isDisplayed = false;
     }
 
     logOut(): void {
