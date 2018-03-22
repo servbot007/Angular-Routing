6b2215ce6d9ef356da793d75c897b0ddeef8dc75
Paul Askegard <angry.twinky@hotmail.com>
2018-03-21 19:28:30 -0500

    Module 7
    Module 7

--- APM-Start/src/app/products/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit.component.ts
@@ -13,8 +13,8 @@ import { ProductService } from './product.service';
 export class ProductEditComponent implements OnInit {
     pageTitle: string = 'Product Edit';
     errorMessage: string;
-
     product: IProduct;
+    private dataIsValid: { [key: string]: boolean } = {};
 
     constructor(private productService: ProductService,
                 private messageService: MessageService,
@@ -53,8 +53,16 @@ export class ProductEditComponent implements OnInit {
         }
     }
 
+    isValid(path: string): boolean {
+        this.validate();
+        if (path) {
+            return this.dataIsValid[path];
+        }
+        return (this.dataIsValid && Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+    }
+
     saveProduct(): void {
-        if (true === true) {
+        if (this.isValid(null)) {
             this.productService.saveProduct(this.product)
                 .subscribe(
                     () => this.onSaveComplete(`${this.product.productName} was saved`),
@@ -73,4 +81,21 @@ export class ProductEditComponent implements OnInit {
         // Navigate back to the product list
         this.router.navigate(['/products']);
     }
+
+    validate(): void {
+        this.dataIsValid = {};
+
+        if (this.product.productName && this.product.productName.length >= 3 && this.product.productCode) {
+            this.dataIsValid['info'] = true;
+        } else {
+            this.dataIsValid['info'] = false;
+        }
+
+        if (this.product.category && this.product.category.length >= 3) {
+            this.dataIsValid['tags'] = true;
+        } else {
+            this.dataIsValid['tags'] = false;
+        }
+    }
+
 }
